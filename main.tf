760e1313abf56a487405175e772729cb2e2d4554
Rob Maynard <robmaynardjr@gmail.com>
2025-05-16 21:17:17 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,6 +10,7 @@ variable "ARM_CLIENT_SECRET" {
     type = string
 }
 provider "azurerm" {
+    features {}
     subscription_id = "51c7239f-55d9-49c2-8878-d9ac2918dc68"
     client_id = "f08630de-a906-4485-8487-4e38240ca663"
     client_secret = var.ARM_CLIENT_SECRET
