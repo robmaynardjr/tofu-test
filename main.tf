2339b3ba724c43caf5f0a320527db9e197288e8c
Rob Maynard <robmaynardjr@gmail.com>
2025-05-16 20:54:11 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ variable {
 provider "azurerm" {
     subscription_id = "51c7239f-55d9-49c2-8878-d9ac2918dc68"
     client_id = "f08630de-a906-4485-8487-4e38240ca663"
-    client_secret = var.client_secret
+    client_secret = var.ARM_CLIENT_SECRET
     tenant_id = "5a2ec06b-b05e-4d96-affa-afc83b2a4629"
 }
 resource "azurerm_resource_group" "rg" {
