283e4c0808544acacb369d8689f40ea810452bc5
Rob Maynard <robmaynardjr@gmail.com>
2025-05-16 21:21:52 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -59,7 +59,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
     }
     admin_ssh_key {
         username = "adminuser"
-        public_key = file("~/.ssh/id_ed25519.pub")
+        public_key = file("~/.ssh/id_rsa.pub")
     }
     source_image_reference {
         publisher = "Canonical"
